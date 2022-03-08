d09d2ffdda4228cd2d356ffd11740c61e3bb81e7
paulopez1330 <49596062+paulopez1330@users.noreply.github.com>
2022-03-08 01:01:04 -0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -119,7 +119,7 @@ class get_data:
                 if df_history is None:
                     exist = False
                 else:
-                    q = df_history.query(f"userid=='{d['userid']}' and professorid=={d['professorid']} and contractid=={d['contractid']} and clubid=={d['clubid']} and date=='{d['date']}'")
+                    q = df_history.query(f"userid=='{d['userid']}' and professorid=='{d['professorid']}' and contractid=={d['contractid']} and clubid=={d['clubid']} and date=='{d['date']}'")
                     exist = len(q) > 0
 
                 if not exist:
@@ -127,6 +127,29 @@ class get_data:
                 else:
                     pass
 
+            elif str(self.path_csv_S3).find('clases_reservas.csv') > 0:
+                if df_history is None:
+                    exist = False
+                else:
+                    q = df_history.query(f"userid=='{d['userid']}' and clubid=={d['clubid']} and date=='{d['date']}' and status=='{d['status']}'")
+                    exist = len(q) > 0
+
+                if not exist:
+                    data_new.append(d)
+                else:
+                    pass
+
+            elif str(self.path_csv_S3).find('clases_reservas.csv') > 0:
+                if df_history is None:
+                    exist = False
+                else:
+                    q = df_history.query(f"userid=='{d['userid']}' and clubid=={d['clubid']} and date=='{d['date']}' and status=='{d['status']}'")
+                    exist = len(q) > 0
+
+                if not exist:
+                    data_new.append(d)
+                else:
+                    pass
         if df_history is None:
             df_new = pd.DataFrame.from_dict(data_new)
             utils.save_csv_gz(df_new, self.path_csv_S3)
@@ -183,8 +206,7 @@ if __name__ == '__main__':
         GF.capture()
         
         path = 's3://karrott-sporlife/raw/clases_reservas.csv.gz'
-        endPoint = "https://sportlifesa.grupodtg.com/api/karrot/getReservesClass"
-    
+        endPoint = "https://sportlifesa.grupodtg.com/api/karrot/getReservesClass"    
         GF = get_data(start=start, end=end, path=path, endPoint=endPoint)
         GF.capture()
     
@@ -200,7 +222,6 @@ if __name__ == '__main__':
         endPoint = "https://sportlifesa.grupodtg.com/api/karrot/getSessionsPT"
         GF = get_data(start=start, end=end, path=path, endPoint=endPoint)
         GF.capture()
-
 
         dateStart = dateStart + timedelta(days=30)
 
